b0be6312cb5fa5d8804172181364a19377eee203
cerebrl <justin@cerebrl.com>
2014-01-23 15:14:11 -0600

    Fixing lazy loading bug if viewport was too narrow for table.

--- _public/dev/js/main.js
+++ _public/dev/js/main.js
@@ -49,11 +49,8 @@ angular.module('TH').
 
 						return (
 							rect.top >= 0 &&
-							rect.left >= 0 &&
 							rect.bottom <= (window.innerHeight ||
-												document.documentElement.clientHeight) &&
-							rect.right <= (window.innerWidth ||
-											   document.documentElement.clientWidth)
+												document.documentElement.clientHeight)
 						);
 					}
 
--- front-end/js/dir-lazy-load.js
+++ front-end/js/dir-lazy-load.js
@@ -24,11 +24,8 @@ angular.module('TH').
 
 						return (
 							rect.top >= 0 &&
-							rect.left >= 0 &&
 							rect.bottom <= (window.innerHeight ||
-												document.documentElement.clientHeight) &&
-							rect.right <= (window.innerWidth ||
-											   document.documentElement.clientWidth)
+												document.documentElement.clientHeight)
 						);
 					}
 
